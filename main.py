10c02c0d14168ac46630dd5930f2cc56e667ae22
Arkadiusz Jaciubek <arkadiusz.jaciubek@hexagon.com>
2025-04-13 13:24:27 +0200

    env configuration

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from settings import Settings
+
+
+def export_envs(environment: str = "dev") -> None:
+    env_file_map = {"dev": ".env.dev", "test": ".env.test", "prod": ".env.prod"}
+
+    env_file = env_file_map.get(environment)
+    if not env_file:
+        raise ValueError(
+            f"Unsupported environment: '{environment}'. Choose from {list(env_file_map.keys())}"
+        )
+
+    if not os.path.exists(env_file):
+        raise FileNotFoundError(f"The environment file '{env_file}' does not exist.")
+
+    load_dotenv(dotenv_path=env_file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Load environment variables from specified.env file."
+    )
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="dev",
+        help="The environment to load (dev, test, prod)",
+    )
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
---
+++ settings.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+from pydantic import validator
+
+
+class Settings(BaseSettings):
+    ENVIRONMENT: str
+    APP_NAME: str
+
+    @validator("ENVIRONMENT")
+    def validate_environment(cls, value):
+        allowed = {"dev", "test", "prod"}
+        if value not in allowed:
+            raise ValueError(f"ENVIRONMENT must be one of {allowed}, got '{value}'")
+        return value
