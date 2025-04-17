154884e3e7c0a9169ebf3ae0f221b183e50cca83
Arkadiusz Jaciubek <arkadiusz.jaciubek@hexagon.com>
2025-04-17 11:06:13 +0200

    add docker

--- training.py
+++ training.py
@@ -2,7 +2,7 @@ from sklearn.datasets import load_iris
 from sklearn import tree
 import joblib
 
-X, y = load_iris(return_X_y=True, as_frame=True)
+X, y = load_iris(return_X_y=True)
 
 model = tree.DecisionTreeClassifier()
 model = model.fit(X, y)
